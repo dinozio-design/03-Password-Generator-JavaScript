497736cf28e14176697eb9ecfd844cec3536e14b
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-08 01:46:00 -0400

    it works

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,31 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-
-const userChoice = {
-  pwLength: "",
-  lowerCase: null,
-  upperCase: null,
-  numbers: null,
-  spChar: null
-};
-
 function writePassword() {
+  
+  var pWord = "";
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var nUM = "0123456789";
+  var characters = "!@#$%^&*";
+  var alphaNumChar = "";
+  var passwordText = document.querySelector("#password");
+  // passwordText.value = pWord;
+
+  const userChoice = {
+    pwLength: "",
+    lowerCase: null,
+    upperCase: null,
+    numbers: null,
+    spChar: null
+  };
   var getLength = function(){
     userChoice.pwLength = window.prompt("Enter Desired Password Length between 8 and 128 character");
     userChoice.pwLength = Number(userChoice.pwLength);
     
   }
-  getLength();
-  // // Add event listener to generate button
-  // generateBtn.addEventListener("click", getLength);
-  
+  getLength(); 
+  // validate input
   while (!userChoice.pwLength || userChoice.pwLength < 8 || userChoice.pwLength >128) {
     window.alert("Please entre numeric value between 8 and 128");
     getLength();
@@ -29,26 +36,35 @@ function writePassword() {
     userChoice.numbers = confirm("Including numbers?");
     userChoice.spChar = confirm("Including special characters?");
   }
+  // validate input
   getType();
+  if (!userChoice.lowerCase&&!userChoice.upperCase&&!userChoice.numbers&&!userChoice.spChar){
+    window.alert("You must choose at lease one option to include");
+    getType();
+  }
+  // set up the character index
+  if (userChoice.lowerCase){
+    alphaNumChar += alpha;
+  }
+  if (userChoice.upperCase){
+    alphaNumChar += ALPHA;
+  }
+  if (userChoice.numbers){
+    alphaNumChar += nUM;
+  }
+  if (userChoice.spChar){
+    alphaNumChar += characters;
+  }
 
+  console.log(alphaNumChar);
+  // articulate the password
+  
+  do {
+    var RAND = Math.floor(Math.random()*alphaNumChar.length);
+    pWord += alphaNumChar[RAND];
+  } while (pWord.length < userChoice.pwLength);
+  // update html element
+  passwordText.value = pWord;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-console.log(typeof userChoice.lowerCase);
-console.log(userChoice.lowerCase.length);
-console.log(userChoice.lowerCase);
-
-
-console.log(typeof userChoice.upperCase);
-console.log(userChoice.upperCase.length);
-console.log(userChoice.upperCase);
-
-console.log(typeof userChoice.numbers);
-console.log(userChoice.numbers.length);
-console.log(userChoice.numbers);
-
-console.log(typeof userChoice.spChar);
-console.log(userChoice.spChar.length);
-console.log(userChoice.spChar);
