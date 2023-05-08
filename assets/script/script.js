8be373d0471eb16a7e60cd36975a9ad58be9e5e5
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-08 00:31:40 -0400

    the code snipets are working in isolation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,76 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var alphaNumChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&* ".split("");
 
+const userChoice = {
+  pwLength: "",
+  lowerCase: null,
+  upperCase: null,
+  numbers: null,
+  spChar: null
+};
 
-var pwObject = {
-  pWord : "",
-
-  prompt1: function(){
-    var userChoiceLength = window.prompt("Enter Desired Password Length between 8 and 128 character");
-    userChoiceLength = Number(userChoiceLength);
-  },
-
-
-
-
-
-
-
-
-  generate: function(){
-    this.prompt1();
-  }
-}
-
-pwObject.generate();
-console.log(pwObject.prompt1.userChoiceLength);
-
-
-
-// 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var alphaNumChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&* ".split("");
-
-
-// Write password to the #password input
 function writePassword() {
-  let pWord = "";
-
-
-  // Prompt for criteria 1 - password length
-  prompt1: function(){
-    var userChoiceLength = window.prompt("Enter Desired Password Length between 8 and 128 character");
-    userChoiceLength = Number(userChoiceLength);
+  var getLength = function(){
+    userChoice.pwLength = window.prompt("Enter Desired Password Length between 8 and 128 character");
+    userChoice.pwLength = Number(userChoice.pwLength);
+    
   }
-  // Validating user input
-  if (!userChoiceLength || userChoiceLength < 8 || userChoiceLength >128) {
-    window.alert("Please entre numeric value between 8 and 128");
-    userChoiceLength;
-  }
-
-  // Prompt for criteria 2 - charType
+  getLength();
+  // // Add event listener to generate button
+  // generateBtn.addEventListener("click", getLength);
   
-
-  // generatePassword: function () {
-  //   var RAND = Math.floor(Math.random()*alphaNumChar.length);
-  //   pWord += alphaNumChar[RAND];
-  //   console.log(pWord);
-  // },
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-
-  // console.log(alphaNumChar[62]);
-
-
-
-  passwordText.value = password;
+  while (!userChoice.pwLength || userChoice.pwLength < 8 || userChoice.pwLength >128) {
+    window.alert("Please entre numeric value between 8 and 128");
+    getLength();
+  }
+  var getType = function(){
+    userChoice.lowerCase = confirm("Including lower case letter?");
+    userChoice.upperCase = confirm("Including upper case letter?");
+    userChoice.numbers = confirm("Including numbers?");
+    userChoice.spChar = confirm("Including special characters?");
+  }
+  getType();
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+console.log(typeof userChoice.lowerCase);
+console.log(userChoice.lowerCase.length);
+console.log(userChoice.lowerCase);
+
+
+console.log(typeof userChoice.upperCase);
+console.log(userChoice.upperCase.length);
+console.log(userChoice.upperCase);
+
+console.log(typeof userChoice.numbers);
+console.log(userChoice.numbers.length);
+console.log(userChoice.numbers);
+
+console.log(typeof userChoice.spChar);
+console.log(userChoice.spChar.length);
+console.log(userChoice.spChar);
